1be86e3468003a1eba8bdeb160aca38e9def8bdf
Carlos Escalona <carlosesca2110@gmail.com>
2025-07-29 02:42:53 -0600

    Add files via upload

---
+++ Practicas/Practica2/AccionInt.java
@@ -0,0 +1,7 @@
+
+import java.awt.event.*;
+public interface AccionInt {
+	public void accion(int n, ActionEvent e);
+}
+
+
---
+++ Practicas/Practica2/Dibujable.java
@@ -0,0 +1,7 @@
+
+
+import java.awt.*;
+
+interface Dibujable {
+	public void dibuja(Graphics g);
+}
